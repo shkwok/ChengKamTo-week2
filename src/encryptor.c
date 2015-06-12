a9206a755145eefe266ee5f8bc57b8903fbdb732
tung1123 <hstung1123@gmail.com>
2015-06-12 10:49:13 +0800

    1.1

--- src/encryptor.c
+++ src/encryptor.c
@@ -1,46 +1,82 @@
-// Author: Peter Membrey
-// Date: 30/05/2015
-// Very simple application to get us started!
-
-/*
- * Note: This approach works because of the way
- * ASCII lays out the character set - this will
- * work with most text, but be warned about UTF-8
- * or non ASCII characters..
- */
-
-
 #include <stdio.h>
-
-char c;
+#include <ctype.h>
+#include <string.h>
 
 int main() {
+    char key[26], map[26][26];
+    char c;
+    int i =0, j =0;
+    for(i = 0;i < 26;i++)
+        for(j = 0;j < 26;j++){
+            map[i][j]= 'A'+i+j > 'Z' ? 64+('A'+i+j-'Z'):'A'+i+j;
+        }
+
+
+    printf("Please enter a key within 26 letters:");
+    scanf("%s",key);
+    getchar();
+    for(i = 0; i < 26 ;i++)
+        key[i] = toupper(key[i]);
+
     do {
-        // Read a character from stdin
-        c = fgetc(stdin);
-        // End of file? Nothing left to read...
-        if (feof(stdin)) {
-            // Exit loop
-            break;
-        }
+        do {
+            printf("\nEnter 1 to encrypt or 2 to decrypt: ");
+            scanf("%c", &c);
 
-        // Is the character lower case?
-            if((c >= 'a') && (c <= 'z') ||(c >= 'A') && (c <= 'Z')) {
-            if ((c >= 'a') && (c <= 'z')) {
-                // Yes, let's make it upper case
-                c -= 32;
-            }
-            if (c - 6 >= 65)
-                c -= 6;
-            else
-                c += 20;
-        }
+        } while (c != '1' && c != '2');
+        getchar();
 
-        // Print the character to stdout (default)
-        printf("%c", c);
-    } while (1);
-    // Exit cleanly
+        if (c == '1') {
+            int currPos = 0;
+            printf("Enter the string: ");
+            do {
+                int row = 0;
+                int col = 0;
+                // Read a character from stdin
+                c = fgetc(stdin);
+                if (('A' <= c && c <= 'Z') || ('a' <= c && c<= 'z')) {
+                    c = toupper(c);
+                    row = key[currPos] - 65;
+                    if(currPos + 1 >= strlen(key))
+                        currPos = 0;
+                    else
+                        currPos += 1;
+                    col = c - 65;
+                    printf("%c", map[row][col]);
+                }
+                else
+                    printf("%c", c);
+
+            } while (c != '\n');
+        }
+        else if( c == '2'){
+            int currPos = 0;
+            printf("Enter the string: ");
+            do {
+                int row = 0;
+                int col = 0;
+                // Read a character from stdin
+                c = fgetc(stdin);
+                if (('A' <= c && c <= 'Z') || ('a' <= c && c<= 'z')) {
+                    c = toupper(c);
+                    row = key[currPos] - 65;
+                    if(currPos + 1 >= strlen(key))
+                        currPos = 0;
+                    else
+                        currPos += 1;
+                    for(col = 0;col < 26;col++){
+                        if(map[row][col] == c)
+                            break;
+                    }
+                    printf("%c", col + 65);
+                }
+                else
+                    printf("%c", c);
+
+            } while (c != '\n');
+
+
+        }
+    }while(1);
     return 0;
-
 }
-
